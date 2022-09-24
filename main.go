a756ceda061afbc680b570f2539d1901c953aab1
LinTrieu <linnatrieu@gmail.com>
2022-09-24 15:51:59 -0700

    adds integration test

--- main.go
+++ main.go
@@ -13,7 +13,13 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
 }
 
+// GetPlayerScore retrieves scores for a given player.
+func (i *InMemoryPlayerStore) RecordWin(name string) {}
+
 func main() {
 	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5001", server))
 }
+
+// Integration tests section
+// TODO https://quii.gitbook.io/learn-go-with-tests/build-an-application/http-server#integration-tests
