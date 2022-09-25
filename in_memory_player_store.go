f2bf04e743c5b550a71c25cc3ea8ee12652becb0
LinTrieu <linnatrieu@gmail.com>
2022-09-24 21:43:23 -0700

    adds mutex to synchronize read/write access

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -1,20 +1,32 @@
 package main
 
-// InMemoryPlayerStore collects data about players in memory.
+import "sync"
+
+// NewInMemoryPlayerStore initialises an empty player store.
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{
+		store: map[string]int{},
+		lock:  sync.RWMutex{},
+	}
 }
 
+// InMemoryPlayerStore collects data about players in memory.
 type InMemoryPlayerStore struct {
 	store map[string]int
+	// A mutex is used to synchronize read/write access to the map
+	lock sync.RWMutex
 }
 
-// RecordWin records a win for a given player.
+// RecordWin will record a player's win.
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	i.store[name]++
 }
 
 // GetPlayerScore retrieves scores for a given player.
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	return i.store[name]
 }
